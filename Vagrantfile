7a8ac92b772876deae827e5523770c7b7bc3677b
Mauricio S <msanchez@aplyca.com>
2016-04-14 14:55:20 -0500

    Improving tests

--- Vagrantfile
+++ Vagrantfile
@@ -18,8 +18,8 @@ Vagrant.configure(2) do |config|
 
       # Asinging a provisioner
       config_machine.vm.provision :ansible, run: "always" do |provisioner|
-          provisioner.playbook = "playbook.yml"
-          provisioner.extra_vars = "custom_vars.yml" if File.file?("custom_vars.yml")
+          provisioner.playbook = "playbooks.yml"
+          provisioner.extra_vars = "custom.yml" if File.file?("custom.yml")
       end
   end
 end
--- tests/tests.sh
+++ tests/tests.sh
@@ -5,13 +5,13 @@ cd $(dirname "$0") && cd ..
 
 echo "Checking syntax"
 
-ansible-playbook -i build/inventory build/playbook.yml --connection=local --syntax-check
+ansible-playbook -i inventories/local playbooks.yml --connection=local --syntax-check
 
 echo "Running role"
-ansible-playbook -i build/inventory build/playbook.yml --connection=local
+ansible-playbook -i inventories/local playbooks.yml --connection=local
 
 echo "Checking idempotence"
-ansible-playbook -i build/inventory build/playbook.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ansible-playbook -i inventories/local playbooks.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 
 echo "Running tests"
-ansible-playbook -i build/inventory tests/playbook.yml --connection=local 
+ansible-playbook -i inventories/local playbooks.yml --connection=local 
